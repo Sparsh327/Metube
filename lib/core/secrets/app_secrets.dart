bb2c7b35683ae9991be4ccc451c252da599b27be
sparshjaiswal <sparsh.jas07@gmail.com>
2025-01-18 21:52:08 +0530

    rrr

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppSecrets {
-  final supabaseUrl = dotenv.env['SUPABASE_URL'] ?? 's';
-  final anonKey = dotenv.env['ANON_KEY'] ?? 's';
+  final supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  final anonKey = dotenv.env['ANON_KEY'] ?? '';
 }
